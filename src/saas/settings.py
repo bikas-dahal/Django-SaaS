6774ee29115072cd9653ec298130861956e295bb
bikky <bikkydahal@gmail.com>
2024-06-16 12:25:28 +0545

    Baisc Neon Integration and Folder restructuring for frontend

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os 
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#u7z40@@&$hl*-0mk6(a%co!q=)0r_ex&$h_35qttod*_ir)-e'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get('DJANGO_DEBUG')).lower() == 'true'
 
+DEBUG = config('DJANGO_DEBUG', cast=bool)
 print('DEBUG', DEBUG, type(DEBUG))
 
   
@@ -93,12 +94,29 @@ WSGI_APPLICATION = 'saas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+CONN_MAX_AGE = config('CONN_MAX_AGE', cast=int, default=30)
+DATABASE_URL = config('DATABASE_URL', cast=str)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(
+            default = DATABASE_URL,
+            conn_health_checks = True,
+            conn_max_age = CONN_MAX_AGE
+        )
+    }
+
 
 
 # Password validation
--- src/visits/views.py
+++ src/visits/views.py
@@ -5,11 +5,23 @@ from .models import PageVisit
 
 
 def index(request):
-
     qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count() * 100.0) / qs.count()
+    except:
+        percent = 0
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "percent": percent,
+        "total_visit_count": qs.count(),
+    }
+    PageVisit.objects.create(path=request.path)
     
 
-    return render(request, 'index.html')
+    return render(request, 'index.html', my_context)
 
 
 def visits(request):
