1f47e033f4f44e6cf21cacc91f4daae41773dbab
bikky <bikkydahal@gmail.com>
2024-06-20 14:06:14 +0545

    Checkin g workflow for dburl

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -167,18 +167,18 @@ DATABASES = {
     }
 }
 
-# CONN_MAX_AGE = config('CONN_MAX_AGE', cast=int, default=30)
-# DATABASE_URL = config('DATABASE_URL', default=None)
+CONN_MAX_AGE = config('CONN_MAX_AGE', cast=int, default=30)
+DATABASE_URL = config('DATABASE_URL', default=None)
 
-# if DATABASE_URL is not None:
-#     import dj_database_url
-#     DATABASES = {
-#         'default': dj_database_url.config(
-#             default = DATABASE_URL,
-#             conn_health_checks = True,
-#             conn_max_age = CONN_MAX_AGE
-#         )
-#     }
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(
+            default = DATABASE_URL,
+            conn_health_checks = True,
+            conn_max_age = CONN_MAX_AGE
+        )
+    }
 
 
 
